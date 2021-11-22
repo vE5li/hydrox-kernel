5e403e3d4ba83ce899a8d7f7cd5d051019eed3a3
ve5li <ve5li@tuta.io>
2021-11-23 00:06:04 +0100

    rustify mem functions

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -2,52 +2,33 @@ pub mod heap;
 
 #[no_mangle]
 pub extern fn memcpy(destination: *mut u8, source: *const u8, length: usize) -> *mut u8 {
-    let mut offset = 0;
-    while offset < length {
-        unsafe { *destination.offset(offset as isize) = *source.offset(offset as isize) };
-        offset += 1;
-    }
+    (0..length).for_each(|offset| unsafe { *destination.offset(offset as isize) = *source.offset(offset as isize) });
     return destination;
 }
 
 #[no_mangle]
 pub extern fn memmove(destination: *mut u8, source: *const u8, length: usize) -> *mut u8 {
-    if source < destination as *const u8 {
-        let mut offset = length;
-        while offset != 0 {
-            offset -= 1;
-            unsafe { *destination.offset(offset as isize) = *source.offset(offset as isize) };
-        }
-    } else {
-        let mut offset = 0;
-        while offset < length {
-            unsafe { *destination.offset(offset as isize) = *source.offset(offset as isize) };
-            offset += 1;
-        }
+    match source < destination as *const u8 {
+        true => (0..length).rev().for_each(|offset| unsafe { *destination.offset(offset as isize) = *source.offset(offset as isize) }),
+        false => { memcpy(destination, source, length); },
     }
     return destination;
 }
 
 #[no_mangle]
 pub extern fn memset(destination: *mut u8, source: i32, length: usize) -> *mut u8 {
-    let mut offset = 0;
-    while offset < length {
-        unsafe { *destination.offset(offset as isize) = source as u8 };
-        offset += 1;
-    }
+    (0..length).for_each(|offset| unsafe { *destination.offset(offset as isize) = source as u8 });
     return destination;
 }
 
 #[no_mangle]
 pub extern fn memcmp(source1: *const u8, source2: *const u8, length: usize) -> i32 {
-    let mut offset = 0;
-    while offset < length {
+    for offset in 0..length {
         let source1 = unsafe { *source1.offset(offset as isize) };
         let source2 = unsafe { *source2.offset(offset as isize) };
         if source1 != source2 {
             return source1 as i32 - source2 as i32
         }
-        offset += 1;
     }
     return 0;
 }
